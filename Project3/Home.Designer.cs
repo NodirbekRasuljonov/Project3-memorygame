c3e90e60a9e976c7fd1be483d975e92a68778543
Nodirbek Rasuljonov <nodirbekmaqsudjonovich@gmail.com>
2023-06-07 15:47:39 +0200

    put random numbers in random places

--- Project3/Home.Designer.cs
+++ Project3/Home.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.backToMainMenu = new System.Windows.Forms.Button();
             this.buttonsTable = new System.Windows.Forms.FlowLayoutPanel();
+            this.startGameButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -41,6 +42,7 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.startGameButton);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.backToMainMenu);
             this.panel1.Location = new System.Drawing.Point(1, 5);
@@ -72,7 +74,7 @@
             // backToMainMenu
             // 
             this.backToMainMenu.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backToMainMenu.Location = new System.Drawing.Point(10, 188);
+            this.backToMainMenu.Location = new System.Drawing.Point(13, 383);
             this.backToMainMenu.Name = "backToMainMenu";
             this.backToMainMenu.Size = new System.Drawing.Size(170, 30);
             this.backToMainMenu.TabIndex = 0;
@@ -87,6 +89,17 @@
             this.buttonsTable.Size = new System.Drawing.Size(502, 440);
             this.buttonsTable.TabIndex = 1;
             this.buttonsTable.Paint += new System.Windows.Forms.PaintEventHandler(this.buttonsTable_Paint);
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startGameButton.Location = new System.Drawing.Point(13, 228);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(167, 23);
+            this.startGameButton.TabIndex = 2;
+            this.startGameButton.Text = "Start Game";
+            this.startGameButton.UseVisualStyleBackColor = true;
+            this.startGameButton.Click += new System.EventHandler(this.startGameButton_Click);
             // 
             // Home
             // 
@@ -112,5 +125,6 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.FlowLayoutPanel buttonsTable;
+        private System.Windows.Forms.Button startGameButton;
     }
 }
--- Project3/Home.cs
+++ Project3/Home.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace Project3
@@ -15,6 +16,7 @@ namespace Project3
     {
         
         Random r = new Random();
+        int indexx = 1;
         public Home()
         {
             InitializeComponent();
@@ -29,10 +31,6 @@ namespace Project3
 
         private void Home_Load(object sender, EventArgs e)
         {
-            List<int> randomRowIndexes = generateRandomIndexes();
-            List<int> randomColumnIndexes = generateRandomIndexes();
-            List<int> randomNumbers=generateRandomIndexes();
-
             buttonsTable.Controls.Clear();
             for (int row = 0; row < 5; row++)
             {
@@ -44,56 +42,106 @@ namespace Project3
                     button.Width = 80;
                     button.Height = 80;
                     button.BackColor = Color.Blue;
-                    Console.WriteLine($"RandomNumbers: {randomNumbers[column].ToString()}");
-                    Console.WriteLine($"RandomINdex{randomRowIndexes[column].ToString()}");
-                    Console.WriteLine($"RandomCOlumn{randomColumnIndexes[column].ToString()}");
-                    if (randomNumbers[row] == row || randomNumbers[column]==column) 
-                    {
-                        button.Text = randomNumbers[column].ToString();
-                    }
-                    else
-                    {
 
-                      // button.Text = "0";
-                    }
-                    
+
                     //button.Text = $"{row}_{column}";
                     buttonsTable.Controls.Add(button);
-                   
+
                 }
+
             }
 
-
         }
-       private List<int> generateRandomIndexes()
-       {
-            List<int> rands = new List<int>();
-            while (rands.Count<5)
-            {
-                int randomNumber = r.Next(1, 6);
-                if (!rands.Contains(randomNumber))
-                {
-                    rands.Add(randomNumber);
-                }
-            }
-            return new List<int>(rands);
-       }
-
-     
 
 
-        private void Button_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            button.Text = button.Name;
-
-            // Perform actions based on the design name of the button
-            // ...
-        }
+       
 
         private void buttonsTable_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void startGameButton_Click(object sender, EventArgs e)
+        {
+            indexx= 1;
+            buttonsTable.Controls.Clear();
+
+            Random random = new Random();
+            //generate random numbers
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 6; i++)
+            {
+                numbers.Add(i);
+            }
+
+
+
+            //generate 5 random places 
+            HashSet<int> randomIndices = new HashSet<int>();
+
+            while (randomIndices.Count < 5)
+            {
+                randomIndices.Add(random.Next(25));
+            }
+
+            //generate buttons with random numbers in random places
+            int index = 0;
+            for (int row = 0; row < 5; row++)
+            {
+                for (int column = 0; column < 5; column++)
+                {
+                    Button button = new Button();
+
+                    button.Name = $"btn_{row}_{column}";
+                    button.Width = 80;
+                    button.Height = 80;
+                    button.BackColor = Color.Blue;
+                    button.Click += Button_Click;
+
+                    if (randomIndices.Contains(row * 5 + column))
+                    {
+                        button.Text = numbers[index].ToString(); // Assign the random number to the button
+                        index++;
+                    }
+                    else
+                    {
+
+                    }
+
+                    buttonsTable.Controls.Add(button);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+           
+            Button button = (Button)sender;
+            button.Text = button.Text;
+            Console.WriteLine(button.Text);
+            
+            if (button.Text == "")
+            {
+                button.BackColor = Color.Red;
+            }
+            else
+            {
+                if (int.Parse(button.Text) == indexx && indexx<6)
+                {
+                    button.BackColor = Color.Green;
+                    button.Enabled = false;
+                    indexx++;
+                }
+                else
+                {
+
+                    button.BackColor = Color.Gray;
+
+                }
+            }
+           
+           
+            Console.WriteLine($"INDEXXXXXXXXXXX {indexx}");
         }
     }
 }
